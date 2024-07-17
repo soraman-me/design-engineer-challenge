d6c242f219ea0924f2b5e9f56e1632f49d70bf6b
soraman-me <31667047+soraman-me@users.noreply.github.com>
2024-07-16 19:11:45 -0700

    Header

---
+++ components/sections/SectionNFTWallpapers.tsx
@@ -0,0 +1,9 @@
+export default function SectionNFTWallpapers() {
+  return (
+    <section>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate porro
+      ea minima obcaecati id. Nam, non quod. Doloribus ducimus iste eaque fugiat
+      laudantium obcaecati, delectus error labore ipsam cum laboriosam.
+    </section>
+  )
+}
